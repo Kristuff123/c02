fcbb5d74f8018a21c945dcccb270f820c3e287f9
kristof <kristuff86@icloud.com>
2024-01-27 20:54:40 +0100

    ex07

---
+++ ex07/ft_strupcase.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kristof <kristof@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/27 20:40:50 by kristof           #+#    #+#             */
+/*   Updated: 2024/01/27 20:53:09 by kristof          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{	
+		str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char	testString[] = "hello world";
+
+	printf("original string is %s\n", testString);
+	ft_strupcase(testString);
+	printf("modified string is %s\n", testString);
+	return (0);
+}
